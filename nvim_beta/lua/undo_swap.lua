109d18ff485e82c6af0c7921316f6302a9d53ccf
GYoung <ytok1108@aol.com>
2024-08-12 22:39:10 +0900

    undo & swap file add

--- nvim_beta/lua/undo_swap.lua
+++ nvim_beta/lua/undo_swap.lua
@@ -1,9 +1,9 @@
 USER = os.getenv("USER")
 local curr_group = vim.fn.system("id -ng 2> /dev/null | tr -d '\n'")
 
-SWAPDIR = "/home/" .. curr_group .. "/" .. USER .. "/nvim_swapfile/swap//"
-BACKUPDIR = "/home/" .. curr_group .. "/" .. USER .. "/nvim_swapfile/backup//"
-UNDODIR = "/home/" .. curr_group .. "/" .. USER .. "/nvim_swapfile/undo//"
+SWAPDIR = "/home/" .. curr_group .. "/" ..  "/nvim_swapfile/swap//"
+BACKUPDIR = "/home/" .. curr_group .. "/" .. "/nvim_swapfile/backup//"
+UNDODIR = "/home/" .. curr_group .. "/" ..  "/nvim_swapfile/undo//"
 
 if vim.fn.isdirectory(SWAPDIR) == 0 then
 	vim.fn.mkdir(SWAPDIR, "p", "0o700")
@@ -18,9 +18,9 @@ if vim.fn.isdirectory(UNDODIR) == 0 then
 end
 
 -- Enable swap, backup, and persistant undo
-vim.opt.directory = "/home/gy/nvim_swapfile/swap"
-vim.opt.backupdir = "/home/gy/nvim_swapfile/backup"
-vim.opt.undodir = "/home/gy/nvim_swapfile/undo"
+vim.opt.directory = SWAPDIR
+vim.opt.backupdir = BACKUPDIR
+vim.opt.undodir = UNDODIR
 vim.opt.swapfile = true
 vim.opt.backup = true
 vim.opt.undofile = true

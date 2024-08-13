ac5306e113a963dc76a8285f8cd04bafe51baaad
GYoung <ytok1108@aol.com>
2024-08-13 20:00:09 +0900

    add gitsign

--- nvim_beta/lua/plugins/gitsigns.lua
+++ nvim_beta/lua/plugins/gitsigns.lua
@@ -45,4 +45,48 @@ require('gitsigns').setup {
   -- yadm                = {
   --   enable = false
   -- },
+  on_attach = function(bufnr)
+    local gitsigns = require('gitsigns')
+
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    -- Navigation
+    map('n', ']c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({']c', bang = true})
+      else
+        gitsigns.nav_hunk('next')
+      end
+    end)
+
+    map('n', '[c', function()
+      if vim.wo.diff then
+        vim.cmd.normal({'[c', bang = true})
+      else
+        gitsigns.nav_hunk('prev')
+      end
+    end)
+
+    -- Actions
+    map('n', 'Ghs', gitsigns.stage_hunk)
+    map('n', 'Ghr', gitsigns.reset_hunk)
+    map('v', 'Ghs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('v', 'Ghr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+    map('n', 'GhS', gitsigns.stage_buffer)
+    map('n', 'Ghu', gitsigns.undo_stage_hunk)
+    map('n', 'GhR', gitsigns.reset_buffer)
+    map('n', 'Ghp', gitsigns.preview_hunk)
+    map('n', 'Ghb', function() gitsigns.blame_line{full=true} end)
+    map('n', 'Gtb', gitsigns.toggle_current_line_blame)
+    map('n', 'Ghd', gitsigns.diffthis)
+    map('n', 'GhD', function() gitsigns.diffthis('~') end)
+    map('n', 'Gtd', gitsigns.toggle_deleted)
+
+    -- Text object
+    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+  end
 }
